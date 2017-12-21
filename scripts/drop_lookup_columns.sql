2d7a12f74328c6ae3e7bdfb50e4290bcc408da05
Dian Fay <dian.m.fay@gmail.com>
2017-12-20 19:22:29 -0500

    formatting

--- scripts/drop_lookup_columns.sql
+++ scripts/drop_lookup_columns.sql
@@ -1,9 +1,9 @@
 set search_path=dox;
 drop function if exists drop_lookup_columns(varchar, varchar);
 create function drop_lookup_columns(
-  collection varchar,
-  schema varchar default 'public',
-  out res bool
+	collection varchar,
+	schema varchar default 'public',
+	out res bool
 )
 as $$
 declare lookup text;
@@ -12,8 +12,8 @@ begin
 										FROM information_schema.columns
 										WHERE table_name=%L AND table_schema=%L AND column_name LIKE %L',
 									collection,schema,'lookup%') loop
-      execute format('alter table %s.%s drop column %I', schema, collection, lookup);
-    end loop;
+			execute format('alter table %s.%s drop column %I', schema, collection, lookup);
+		end loop;
 
 		res := true;
 end;
