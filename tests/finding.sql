a82e011ea72c8ca0d600f3798de144d93e5850c1
Rob Conery <rob@conery.io>
2017-12-21 15:51:33 -0800

    Added tests for finds and search

--- tests/finding.sql
+++ tests/finding.sql
@@ -13,9 +13,8 @@ select * into res from dox.get(collection => 'customers', id => 1);
 assert res ->> 'id' = '1';
 assert res ->> 'name' = 'chuck';
 
-
-raise notice 'Looking up customers by id returns';
-select * into res from dox.get(collection => 'customers', id => 1);
+raise notice 'Looking up one customer by term returns';
+select * into res from dox.find_one(collection => 'customers', term =>'{"name":"chuck"}');
 
 assert res ->> 'id' = '1';
 assert res ->> 'name' = 'chuck';
@@ -27,6 +26,21 @@ select count(1) into found from dox.find(
 );
 
 assert found = 2, 'Bad find';
+
+raise notice 'Looking up using fuzzy returns 2';
+select count(1) into found from dox.fuzzy(
+  collection => 'customers', 
+  key => 'company',
+  term => 'red'
+);
+assert found = 2, 'Bad fuzz';
+
+raise notice 'Looking up using search returns 2';
+select count(1) into found from dox.search(
+  collection => 'customers', 
+  term => 'red4'
+);
+assert found = 2, 'Bad search';
 
 end;
 $$ language plpgsql;
